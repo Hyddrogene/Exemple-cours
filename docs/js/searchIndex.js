a459c43aeeb21e559005b1af4d5c8bb02a6fe9b2
Hyddrogene <Hyddrogene>
2025-12-02 14:48:25 +0100

    Message 4

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\add\u0028\u0029",
+            "name": "add",
+            "summary": "Additionne\u0020deux\u0020entiers.",
+            "url": "namespaces/default.html#function_add"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
