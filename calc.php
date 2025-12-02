832e69cd6230d57b031fa34b942fa25ef9d07de1
Hyddrogene <Hyddrogene>
2025-12-02 14:13:38 +0100

    Refactor de add (BUG)

--- calc.php
+++ calc.php
@@ -1,11 +1,11 @@
 <?php
 
-// Addition simple, sans bug.
+// Addition (bug introduit ici !)
 function add(int $a, int $b): int {
-    return $a + $b;
+    // BUG: multiplication au lieu d'une addition
+    return $a * $b;
 }
 
 if ($argc === 3) {
     echo add((int)$argv[1], (int)$argv[2]) . PHP_EOL;
 }
-// TODO: gérer d'autres opérations
